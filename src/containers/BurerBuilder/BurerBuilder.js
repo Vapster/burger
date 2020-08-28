b2203f1a54ff0d97c1e0ea20d320a07b60d4bfe3
Vapster <varshilpatel1498@gmail.com>
2020-08-28 18:37:09 -0400

    post request add for order and spinner add

--- src/containers/BurerBuilder/BurerBuilder.js
+++ src/containers/BurerBuilder/BurerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BurgerControls from '../../components/Burger/BurgerControls/BurgerControls';
 import Modal from '../../components/UI/Modal/Modal';
 import BurgerSummary from '../../components/Burger/BurgerSummary/BurgerSummary';
+import axiosInstance from '../../axios-orders'
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -22,7 +24,8 @@ class BurgerBuilder extends Component {
             salad:0
         },
         totalPrice: 4,
-        showSummary: false
+        showSummary: false,
+        loading: false
     }
 
     handleToggleShowSummary = () => {
@@ -31,7 +34,34 @@ class BurgerBuilder extends Component {
     }
 
     handleCheckOut = () => {
-        alert("This one is on us.")
+
+        this.setState({loading: true})
+        console.log("here")
+
+        const data = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customeer: {
+                name: 'Varshil Patel',
+                email: 'varshilpatel1498@gmail.com',
+                address: {
+                    number: '1478',
+                    street: 'dontKnow st east',
+                    state: 'newOne',
+                    zipcode: '234567'
+                }
+            },
+            deliveryMethod: 'fastest'
+        };
+        
+        axiosInstance.post('/orders.json', data)
+        .then((res) => {
+            this.setState({loading: false, showSummary: false})
+            console.log('done')
+        })
+        .catch((error)=>{
+            this.setState({loading: false, showSummary: false})
+        })
     }
 
     handleIncreaseIngredient = (ingredient) => {
@@ -71,12 +101,18 @@ class BurgerBuilder extends Component {
             return null;
         })
 
+        let orderSummary = <BurgerSummary totalPrice={this.state.totalPrice} onCheckOut={this.handleCheckOut} onCancel={this.handleToggleShowSummary} ingredients={this.state.ingredients} />
+
+        if (this.state.loading){
+            orderSummary = <Spinner/>
+        }
+
         return (
             <Aux>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BurgerControls orderNowClicked={this.handleToggleShowSummary} price={this.state.totalPrice} disableOrderNow={this.state.totalPrice.toFixed(2)<=4} disabledButtons={disabledButtons} increaseIngredient={this.handleIncreaseIngredient} DecreaseIngredient={this.handleDecreaseIngredient} />
                 <Modal onclickBackdrow={this.handleToggleShowSummary} show={this.state.showSummary}>
-                    <BurgerSummary totalPrice={this.state.totalPrice} onCheckOut={this.handleCheckOut} onCancel={this.handleToggleShowSummary} ingredients={this.state.ingredients} />
+                    {orderSummary}
                 </Modal>
             </Aux>
         );
